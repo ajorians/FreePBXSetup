0505774a8d3230a68ce5e31dd65aee3b84159dd3
A.J. Orians <ajorians@raspberry400.local>
2024-09-07 23:16:03 -0400

    Using a package.

---
+++ utils.ts
@@ -0,0 +1,22 @@
+export function delay(time) {
+   return new Promise(function(resolve) {
+       setTimeout(resolve, time)
+   });
+}
+
+export async function takeScreenshot(page, filename) {
+   if( !page ) {
+      console.log("Page is NULL.  Cannot continue");
+      return;
+   }
+
+   const path = "/srv/www/htdocs/allwrite/";
+   const screenshotPath = path + filename;
+
+   console.log("Taking screenshot: " + screenshotPath);
+
+   await page.screenshot({
+     path: screenshotPath
+   });
+}
+
