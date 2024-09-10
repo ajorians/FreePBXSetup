a741973e37bd7f25445780bbe57cbfe6a70707f8
A.J. Orians <ajorians@raspberry400.local>
2024-09-09 23:06:52 -0400

    Added first extension.

--- extensions.ts
+++ extensions.ts
@@ -36,7 +36,7 @@ async function getLinkWithText(page, text) {
    const links = await page.$$('a'); // Find all links on the page
 
    for(const link of links) {
-      const textContent = await link.evaluate(el => el.textContent);
+      const textContent = await link.evaluate(el => el.textContent.trim());
       //console.log("Link text: " + textContent);
       if( textContent === text ) {
          return link;
@@ -52,8 +52,8 @@ async function getButtonsWithText(page, text) {
   // Filter and click the button based on its text content
   for (let button of buttons) {
     const buttonText = await page.evaluate(el => el.textContent.trim(), button);
+    //console.log("ButtonText: " + buttonText);
     if (buttonText.includes( text )) { // Replace with the text you're looking for
-      console.log("Foun button: " + buttonText );
       return button;
     }
   }
@@ -68,9 +68,6 @@ export async function addExtensions(page)
       return false;
    }
 
-   //const links = await describeElementsByType(page, 'a');
-   //console.log(links);
-
    // Click Connectivity > Extensions
    const connectivityLink = await getLinkWithText(page, 'Connectivity');
    connectivityLink.hover();
@@ -84,12 +81,48 @@ export async function addExtensions(page)
  
    await utils.takeScreenshot(page, "extensions.png");
 
-   const button = await getButtonsWithText(page, "Add Extension");
-   await button.click();
+   const addExtButton = await getButtonsWithText(page, "Add Extension");
+   await addExtButton.click();
 
    await utils.delay(4000);
 
    await utils.takeScreenshot(page, "addextensions.png");
+
+   //const links = await describeElementsByType(page, 'a');
+   //console.log(links);
+
+   const addSIPExtensionLink = await getLinkWithText(page, 'Add New SIP [chan_pjsip] Extension');
+   addSIPExtensionLink.click();
+
+   await utils.delay(4000);
+
+   await utils.takeScreenshot(page, "addsipextensions.png");
+
+   const userExtensionElement = await page.$('input[type="text"][name="extension"]');
+
+   if( !userExtensionElement )
+   {
+      console.log("No extension element");
+      return false;
+   }
+
+   await userExtensionElement.focus();
+   await userExtensionElement.type('201');
+
+   const displayNameElement = await page.$('input[type="text"][name="name"]');
+
+   if( !displayNameElement )
+   {
+      console.log("No display name element");
+      return false;
+   }
+
+   await displayNameElement.focus();
+   await displayNameElement.type('Desktop');
+
+   await utils.takeScreenshot(page, "addedextensions.png");
+
+   await page.click('#submit');
 
    return true;
 }
