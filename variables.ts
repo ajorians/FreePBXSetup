a2195255c1b80130b49f4806f6de7e00ad90c9e1
A.J. Orians <ajorians@gmail.com>
2025-01-21 22:45:45 -0500

    Cleared variables.  This will be filled in via Ansible.

--- variables.ts
+++ variables.ts
@@ -8,11 +8,8 @@ export const siptrunks = [
 
 //Number + name + password
 export const deviceextensions = [
-["201", "Desktop", "fakepassword"],
-["202", "A.J. IPhone", "fakepassword"]
 ];
 
 //Number + name + extensions
 export const ringgroups = [
-["290", "Test Ring Group", ["201", "202"]]
 ];
